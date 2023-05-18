37726ca075100af96bb48a3a6e7e57c9b67136c5
HTC-zz <zzhtc1907@gmail.com>
2023-05-18 23:54:50 +0700

    CongHT

--- src/javaapplication4/Bai3.java
+++ src/javaapplication4/Bai3.java
@@ -3,12 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication4;
-
-/**
- *
- * @author Acer
- */
-public class Bai3 {
-    
+import java.util.Scanner;
+public class bai3 {
+    public static int fatorial(int n){
+        int i,fatorial = 1;
+        for(i = 1; i <= n; i++){
+            fatorial *= i;
+        }
+        return fatorial;
+    }
+    public static int check(int n){
+        int k = n, sum = 0;
+        while(k > 0){
+            int digit = k % 10;
+            sum += fatorial(digit);
+            k = k / 10;
+        }
+        return sum;
+    }
+   public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       int n;
+       System.out.println("Enter n: "); 
+       n = sc.nextInt();
+       if(n == check(n)){
+           System.out.println(n + " is a Peterson number"); 
+       }
+       else{
+           System.out.println(n + "is not a Peterson number"); 
+       }
+   }
 }
--- src/javaapplication4/Bai4.java
+++ src/javaapplication4/Bai4.java
@@ -1,25 +1,34 @@
-
 import java.util.Scanner;
 
-public class JavaApplication4 {
+public class bai4 {
+
+    public static int fatorial(int n) {
+        int i, fatorial = 1;
+        for (i = 1; i <= n; i++) {
+            fatorial *= i;
+        }
+        return fatorial;
+    }
+
+    public static int check(int n) {
+        int k = n, sum = 0;
+        while (k > 0) {
+            int digit = k % 10;
+            sum += fatorial(digit);
+            k = k / 10;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a, b, c;
-        System.out.println("Enter number a: ");
-        a = sc.nextInt();
-        System.out.println("Enter number b: ");
-        b = sc.nextInt();
-        System.out.println("Enter number c: ");
-        c = sc.nextInt();
-
-        int max = a;
-        if (a < b) {
-            max = b;
+        int i, n;
+        System.out.println("Enter n: ");
+        n = sc.nextInt();
+        for (i = 1; i <= n; i++) {
+            if (i == check(i)) {
+                System.out.println("perfect numbers are: " + i);
+            }
         }
-        if (max < c) {
-            max = c;
-        }
-
-        System.out.println("MAX = " + max);
     }
 }
